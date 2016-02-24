79ae6a3450f7ff2a5f447e8e80588248a38bc1ec
zofy <zofy11@gmail.com>
2016-02-24 10:16:15 +0100

    .js - ws funkcne posielanie sprav servru pribudol server.py a v kniznici websocket-server

--- TicTacToe/settings.py
+++ TicTacToe/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ttt',
-    'ws4redis',
+    # 'ws4redis',
+    # 'socket_server',
 ]
 
 MIDDLEWARE_CLASSES = [
--- ttt/static/ttt/ttt.js
+++ ttt/static/ttt/ttt.js
@@ -1,53 +1,68 @@
 /**
  * Created by Patrik on 20. 2. 2016.
  */
+
+var ws = new WebSocket('ws://localhost:9000/')
 var game = {};
 
-    game.squares = document.querySelectorAll('.square');
+    game.squares = [].slice.call(document.querySelectorAll('.square'));
     game.colors = []; // colors of squares
     game.nums = []; // array for nums in rgb color
-    game.playerSquare = document.querySelector('.player'); // color of the player
+    game.playerSquare = document.querySelector('.player'); // square color of the player
+    game.playerColor = ''; //color of player
+    game.squaresOfPlayer = [];
 
 
     game.init = function(){
-        this.generateColors();
         this.setUpSquares();
-    }
-
-    game.generateColors = function(){
-        for(i = 0; i < this.squares.length; i++){
-            for(j = 0; j < 3; j++){
-                this.nums.push(Math.round(256*Math.random()));
-            }
-            this.colors[i] = 'rgb(' + this.nums[0] + ', ' + this.nums[1] + ', ' + this.nums[2] + ')';
-            this.nums.length = 0;
-        }
     }
 
     game.randomColor = function(){
         return 'rgb(' + Math.round(256*Math.random()) + ', ' + Math.round(256*Math.random()) + ', ' + Math.round(256*Math.random()) + ')';
     }
 
-    game.playerColor = function(){
-        return this.playerSquare.style.background;
+    game.stylePlayersSquares = function(color){
+        this.squaresOfPlayer.forEach(function(square){
+            square.style.background = color;
+        });
+    }
+
+    game.checkSquaresColor = function(color){
+        this.squares.forEach(function(square){
+            if(square.classList[0] !== 'player') {
+                while (square.style.background === color) {
+                    square.style.background = game.randomColor();
+                }
+            }
+        });
     }
 
     game.changeColor = function(square){
         if(square.classList[0] === 'player') {
-            square.style.background = this.randomColor();
+            var color = this.randomColor();
+            this.playerColor = color;
+            square.style.background = color;
+            this.stylePlayersSquares(color);
+            this.checkSquaresColor(color);
         }else{
-            square.style.background = this.playerColor();
+            this.checkSquaresColor(this.playerColor);
+            square.style.background = this.playerColor;
+            this.squaresOfPlayer.push(square);
+            this.squares.splice(game.squares.indexOf(square), 1);
+            square.classList.add('noEvent');
         }
     }
 
     game.setUpSquares = function(){
-        this.squares[this.squares.length] = this.playerSquare;
-        for(i = 0; i < this.squares.length + 1; i++){
-            this.squares[i].style.background = this.randomColor();
-            this.squares[i].addEventListener('click', function(){
+        this.squares.push(this.playerSquare);
+        this.squares.forEach(function(square){
+            square.style.background = game.randomColor();
+            square.addEventListener('click', function(){
+                ws.send(square);
                 game.changeColor(this);
-            })
-        }
+            });
+        });
+        this.playerColor = this.playerSquare.style.background;
     }
 
 
