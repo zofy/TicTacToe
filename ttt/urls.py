d3841325d39dd41ab1b8f472ce6a3d2bdb78ed04
zofy <zofy11@gmail.com>
2016-03-03 11:22:15 +0100

    views.py - pridana session na overovanie ci je user logged in

--- ttt/urls.py
+++ ttt/urls.py
@@ -6,7 +6,11 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^scores/$', views.show_scores, name='scores'),
     url(r'^(?P<size>[0-9]+)/$', views.game, name='game'),
+
     url(r'^login/$', views.login, name='login'),
     url(r'^auth/$', views.auth_view, name='authentication'),
     url(r'^invalid/$', views.invalid, name='invalidLogin'),
+    url(r'^logout/$', views.logout, name='logout'),
+
+    url(r'^menu/$', views.menu, name='menu'),
 ]
--- ttt/views.py
+++ ttt/views.py
@@ -1,7 +1,19 @@
-from django.contrib.auth import authenticate
+# from django.contrib.auth import authenticate
 from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from models import Score, Player
+
+
+def authenticate(username, password):
+    try:
+        return Player.objects.get(name=username, password=password)
+    except:
+        return None
+
+
+def check_session(request):
+    return 'user' not in request.session
 
 
 def home(request):
@@ -10,8 +22,13 @@ def home(request):
 
 def game(request, size):
     listSize = range(0, int(size) ** 2)
+
+    if check_session(request):
+        return HttpResponseRedirect('/ttt/login/')
+
     return render(request, 'ttt/board.html',
-                  {'size': listSize, 'width': 90.0 / int(size), 'margin': 10.0 / (int(size) * 2)})
+                  {'size': listSize, 'width': 90.0 / int(size), 'margin': 10.0 / (int(size) * 2),
+                   'users': request.session['user']})
 
 
 def show_scores(request):
@@ -26,13 +43,14 @@ def login(request):
 def auth_view(request):
     username = request.POST.get('username', '')
     password = request.POST.get('password', '')
+    user = authenticate(username, password)
 
-    try:
-        user = Player.objects.get(name=username, password=password)
-    except:
-        user = None
+    # user = User.objects.create_user(username, password)
+    # user.save()
+    # return HttpResponseRedirect('/ttt/3/')
 
     if user is not None:
+        request.session['user'] = user.name
         return HttpResponseRedirect('/ttt/3/')
     else:
         return HttpResponseRedirect('/ttt/invalid/')
@@ -40,3 +58,15 @@ def auth_view(request):
 
 def invalid(request):
     return render(request, 'ttt/login.html', {'appendix': 'Invalid input, try again!'})
+
+
+def logout(request):
+    try:
+        del request.session['user']
+    except:
+        pass
+    return render(request, 'ttt/login.html', {'appendix': 'You have successfully been logged out!'})
+
+
+def menu(request):
+    pass
