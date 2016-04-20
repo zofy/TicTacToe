ba8f543f5196c89b3e3ea6437fe18e36c27865ee
zofy <zofy11@gmail.com>
2016-04-20 09:45:42 +0200

    testinFabric

--- fabfile.py
+++ fabfile.py
@@ -10,3 +10,31 @@ def backup():
     local('git commit -m "%s"' % comment)
 
     local('git push')
+
+
+def switch_debug(what_to_change, change_to):
+    local('cp TicTacToe/local_settings.py TicTacToe/local_settings.bak')
+    sed = "sed 's/^DEBUG = %s$/DEBUG = %s/' TicTacToe/local_settings.bak > TicTacToe/local_settings.py"
+    local(sed % (what_to_change, change_to))
+    local('rm TicTacToe/local_settings.bak')
+
+
+def deploy():
+    local('pip freeze > requirements.txt')
+    local('git pull')
+    local('git add .')
+
+    print('enter your commit comment: ')
+    comment = raw_input()
+    local('git commit -m "%s"' % comment)
+
+    local('git push')
+
+    switch_debug('True', 'False')
+    local('python manage.py collectstatic')
+    switch_debug('False', 'True')
+
+    local('heroku maintenance:on')
+    local('git push heroku')
+    local('heroku run python manage.py migrate')
+    local('heroku maintenance:off')
