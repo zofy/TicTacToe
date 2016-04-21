71d5e40fca1c6eba1e685dd8c38dbee596ec2ebe
zofy <zofy11@gmail.com>
2016-04-21 19:57:05 +0200

    upravy

--- TicTacToe/wsgi.py
+++ TicTacToe/wsgi.py
@@ -11,9 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TicTacToe.settings")
 os.environ['DJANGO_SETTINGS_MODULE'] = 'TicTacToe.settings'
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
+# application = DjangoWhiteNoise(application)
