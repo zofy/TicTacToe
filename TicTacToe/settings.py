ad7a3d86638a0b9968dea0f2e15fd4bd67822ad8
zofy <zofy11@gmail.com>
2016-04-21 17:59:26 +0200

    upravy

--- TicTacToe/settings.py
+++ TicTacToe/settings.py
@@ -159,5 +159,6 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    DEFAULT_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
     STATIC_URL = S3_URL
